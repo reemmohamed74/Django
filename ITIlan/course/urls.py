7a6bae68acf7dc2e2b75df2fa51fc0a11b037263
reemmohamed74 <reem74mohamed@gmail.com>
2025-03-11 21:57:07 +0200

    Delete ITIlan directory

--- ITIlan/course/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import course_list , delete_course
-urlpatterns = [
-    path('', course_list, name='trainee_list'),
-    path('delete/<int:course_id>/', delete_course, name='delete_course'),
-]
