945e289f9dbdef63cd820af391347931a96f4c54
Reem <reem@PClocalhost.localdomain>
2025-03-12 14:40:32 +0200

    Course and Trainee views created

--- Day3/ITIlan/course/views.py
+++ Day3/ITIlan/course/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .forms import Courseform
 
 # Create your views here.
+def add_course(req):
+    form = Courseform(req.POST)
+    if form.is_valid():
+        form.save()
+        return redirect ('course_list')
+    return render(req,'course/add_course.html')
--- Day3/ITIlan/trainee/views.py
+++ Day3/ITIlan/trainee/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from .forms import Traineeform
+from .models import Trainee
 # Create your views here.
+def add_trainee(req):
+    if req.method =="POST":
+        from = Traineeform(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('trainee_list')
+    return render(req , 'trainee/add_trainee.html')
