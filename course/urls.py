9da4356f9e15957bf7c6c9f0507ab46e38fd5b73
Reem Mohamed <reem74mohamed@gmail.com>
2025-03-17 17:55:49 +0200

    Delete course directory

--- course/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path
-# from . import views
-#
-# urlpatterns = [
-#     path('courses/', views.CourseListView.as_view(), name='course-list'),
-#     path('courses/add/', views.AddCourseView.as_view(), name='add-course'),
-# ]
